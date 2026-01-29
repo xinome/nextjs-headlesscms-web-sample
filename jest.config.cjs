ba99e8fc38169dda3a24961b06b2bacc28d0b174
xinome <xinome2038@gmail.com>
2026-01-29 18:36:35 +0900

    テスト自動化 jest導入

---
+++ jest.config.cjs
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)
+
